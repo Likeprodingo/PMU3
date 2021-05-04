c52b7c676accf41b7ebd5b2c11cb657db076bd4e
Likeprodingo <alshiba1@mail.ru>
2021-05-04 17:59:45 +0300

    Main Activity

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -12,6 +12,10 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(savedInstanceState == null)
+        {
+            getSupportFragmentManager().beginTransaction().add(R.id.MainFragment, new LogIn()).commit(); //Устанавливаем в Activity фрагмент в входом
+        }
     }
 
 }
